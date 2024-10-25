d29da88de41f72cd8068f96ed9b4bee8c8863954
venkatesh8686 <83954722+venkatesh8686@users.noreply.github.com>
2024-10-25 10:54:30 +0530

    adding new file

---
+++ install-kubectl.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+R="\e[31m"
+
+curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv kubectl /usr/local/bin/kubectl
+kubectl version "$R Kubectl installed " 
+
+echo "started installing eksctl"
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+sudo mv /tmp/eksctl /usr/local/bin
+eksctl version "$R eksctl installed"
+
+aws configure
